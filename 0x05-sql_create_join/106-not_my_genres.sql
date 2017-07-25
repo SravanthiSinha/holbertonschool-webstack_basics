1e1dd3111ed59ae39e96270bdcd9db0256e8b17c
sravanthisinha <sravani.sinha@gmail.com>
2017-07-25 16:43:22 -0700

    finish task 106-not_my_genres.sql

--- 0x05-sql_create_join/106-not_my_genres.sql
+++ 0x05-sql_create_join/106-not_my_genres.sql
@@ -1,5 +1,5 @@
 -- list all genres not linked to the show Dexter
-SELECT DISTINCT name from tv_genres where name not in(
+SELECT DISTINCT name FROM tv_genres WHERE name NOT IN(
 SELECT tv_genres.name  FROM tv_genres
 INNER JOIN tv_show_genres ON tv_genres.id = genre_id
 INNER JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id and title = 'Dexter')

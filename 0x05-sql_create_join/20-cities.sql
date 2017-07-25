2d458c39420d294288018ecdf011154aef4cd801
sravanthisinha <sravani.sinha@gmail.com>
2017-07-25 14:16:03 -0700

    finish task 20-cities.sql

---
+++ 0x05-sql_create_join/20-cities.sql
@@ -0,0 +1,11 @@
+-- creates the database hbtn_0d_usa and the table cities (in the database hbtn_0d_usa)
+-- cities :
+-- id INT unique, auto generated, can't be null and is a primary key
+-- state_id INT, can't be null and must be a FOREIGN KEY that references to id of the states table
+-- name VARCHAR(256) can't be null
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	state_id INT, 
+	name VARCHAR(256) NOT NULL,
+	FOREIGN KEY (state_id) REFERENCES hbtn_0d_usa.states(id));

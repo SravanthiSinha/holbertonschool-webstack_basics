668f89c5a45dbc69f0270a6f74edafdad9817521
sravanthisinha <sravani.sinha@gmail.com>
2017-07-25 13:50:43 -0700

    finish task 16-force_name.sql

---
+++ 0x05-sql_create_join/16-force_name.sql
@@ -0,0 +1,3 @@
+-- creates the table force_name on your MySQL server.
+-- force_name to have id INT, name VARCHAR(256) NOT NULL
+CREATE TABLE IF NOT EXISTS force_name (id INT, name VARCHAR(256) NOT NULL);

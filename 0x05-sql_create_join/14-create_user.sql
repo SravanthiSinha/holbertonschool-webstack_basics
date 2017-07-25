0f02e53a68e4f6970ca029680f5604ecc186bd30
sravanthisinha <sravani.sinha@gmail.com>
2017-07-25 13:45:42 -0700

    finish task 14-create_user.sql

---
+++ 0x05-sql_create_join/14-create_user.sql
@@ -0,0 +1,8 @@
+--  creates the MySQL server user user_0d_1
+DROP USER IF EXISTS 'user_0d_1'@'localhost';
+DROP USER IF EXISTS 'user_0d_1'@'%';
+CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+CREATE USER 'user_0d_1'@'%' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'%';
+FLUSH PRIVILEGES;
